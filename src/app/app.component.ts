944ad81fa7feca6134847bad8e61f8bbe5abfecc
Arminas Strolys <arminas.strolys@gmail.com>
2023-02-15 10:36:23 +0200

    Pipes progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string;
+  date!: string;
+  amount!: number;
+
+  tester = {
+    test1: 'TTT',
+    test2: 'VVV',
+    test3: 'CCC',
+  };
+
+  onNameChange(value: Event) {
+    const target = value.target as HTMLInputElement;
+    this.name = target.value;
+  }
+  onDateChange(value: Event) {
+    const target = value.target as HTMLInputElement;
+    this.date = target.value;
+  }
+  onAmountChange(value: Event) {
+    const target = value.target as HTMLInputElement;
+    this.amount = +target.value;
+  }
 }
